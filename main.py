1c9d7615bd6e3c923afef7137536217c3a3508b9
letoufasalaheddine-oss <letoufasalaheddine@gmail.com>
2026-07-17 23:00:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,21 @@ headers = {
 }
 
 response = requests.get(URL, headers=headers)
+response.encoding = "utf-8"
+
+html = response.text
 
 print("الحالة:", response.status_code)
 
-print(response.text[:3000])
+print("هل يوجد منصات دفع الحقوق؟")
+print("منصات دفع الحقوق" in html)
+
+print("هل يوجد frais2026؟")
+print("frais2026" in html)
+
+
+# استخراج جزء الإعلان إن وجد
+pos = html.find("frais2026")
+
+if pos != -1:
+    print(html[pos-500:pos+500])
